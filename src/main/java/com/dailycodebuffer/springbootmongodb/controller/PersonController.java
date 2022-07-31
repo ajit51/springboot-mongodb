ff172d95dc4c4987440e38f7c4cef7b534bb0dc7
ajit51 <ajitk433@gmail.com>
2022-07-31 15:48:23 +0530

    added searchPerson for all fields using mongo template

--- src/main/java/com/dailycodebuffer/springbootmongodb/controller/PersonController.java
+++ src/main/java/com/dailycodebuffer/springbootmongodb/controller/PersonController.java
@@ -3,6 +3,9 @@ package com.dailycodebuffer.springbootmongodb.controller;
 import com.dailycodebuffer.springbootmongodb.collection.Person;
 import com.dailycodebuffer.springbootmongodb.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,5 +36,18 @@ public class PersonController {
     public List<Person> getByPersonAge(@RequestParam Integer maxAge,
                                        @RequestParam Integer minAge){
         return personService.getByPersonAge(maxAge, minAge);
+    }
+
+    @GetMapping("/search")
+    public Page<Person> searchPerson(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) Integer minAge,
+            @RequestParam(required = false) Integer maxAge,
+            @RequestParam(required = false) String city,
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "5") Integer size
+    ){
+        Pageable pageable = PageRequest.of(page, size);
+        return personService.serach(name, minAge, maxAge, city, pageable);
     }
 }
--- src/main/java/com/dailycodebuffer/springbootmongodb/service/PersonService.java
+++ src/main/java/com/dailycodebuffer/springbootmongodb/service/PersonService.java
@@ -1,6 +1,8 @@
 package com.dailycodebuffer.springbootmongodb.service;
 
 import com.dailycodebuffer.springbootmongodb.collection.Person;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -14,4 +16,6 @@ public interface PersonService {
     void delete(String id);
 
     List<Person> getByPersonAge(Integer maxAge, Integer minAge);
+
+    Page<Person> serach(String name, Integer minAge, Integer maxAge, String city, Pageable pageable);
 }
